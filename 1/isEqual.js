68a90f4f128aa062bea24bbb664d3db82472401c
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2019-12-24 12:16:39 +0300

    fix: error when the rhs argument is null

--- 1/isEqual.js
+++ 1/isEqual.js
@@ -3,7 +3,7 @@
 function isEqual(lhs, rhs) {
     for (let field in lhs) {
         if (typeof(lhs[field]) === "object") {
-            if (!isEqual(lhs[field], rhs[field])) {
+            if (!rhs || !isEqual(lhs[field], rhs[field])) {
                 return false;
             }
         }
@@ -14,7 +14,11 @@ function isEqual(lhs, rhs) {
     return true;
 }
 
-let a = isEqual({a: { b: 1 }}, {a: { b: 1 }}); // true
-let b = isEqual({a: { b: 1, c: 1}},{a: { b: 1}}); // false
+const a = isEqual({a: { b: 1 }}, {a: { b: 1 }}); // true
+const b = isEqual({a: { b: 1, c: 1}},{a: { b: 1}}); // false
+const c = isEqual({}, null); // true
+const d = isEqual({a: null}, null); // false
+const e = isEqual({a: null}, {a: {}}); // true
+const f = isEqual({a: []}, {a: {}}); // true
 
-alert(String(a) + " - " + String(b));
+alert(String(a) + " - " + String(b) + " - " + String(c) + " - " + String(d) + " - " + String(e) + " - " + String(f));
